db5ba7d69d6112e768b7db3afae7c8236661ec90
Kosta-Stajic <kostastajic@gmail.com>
2025-05-08 15:02:56 +0200

    bug fixes

--- src/todo.js
+++ src/todo.js
@@ -1,29 +1,28 @@
 
-
 class Todo {
-    constructor (title, description, priority, dueDate, taskId = Date.now()){
-        this.title = title;
-        this.description = description
-        this.priority = priority;
-        this.dueDate = dueDate;
-        this.taskId = taskId;
-        this.isCompleted = false
+    constructor(title, description, priority, dueDate, taskId = Date.now()) {
+      this.title = title;
+      this.description = description;
+      this.priority = priority;
+      this.dueDate = dueDate;
+      this.taskId = taskId;
+      this.isCompleted = false;
     }
     markCompleted() {
-        if (this.isCompleted === false){
-        this.isCompleted = true
-        } else {
-            this.isCompleted = false;
-        }
+      if (this.isCompleted === false) {
+        this.isCompleted = true;
+      } else {
+        this.isCompleted = false;
+      }
     }
-    
-    editDetails (title,description,priority,dueDate){
-        this.title = title;
-        this.description = description
-        this.priority = priority;
-        this.dueDate = dueDate
+  
+    editDetails(title, description, priority, dueDate) {
+      this.title = title;
+      this.description = description;
+      this.priority = priority;
+      this.dueDate = dueDate;
     }
-}
-
-
-export {Todo}
+  }
+  
+  export { Todo };
+  
